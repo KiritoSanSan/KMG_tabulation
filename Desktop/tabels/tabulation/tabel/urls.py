a511e099e5e67dc5fd58bf92607a8337492f7de8
nurzhvn52 <160860126+nurzhvn52@users.noreply.github.com>
2024-03-18 11:21:46 +0500

    Delete Desktop directory

--- Desktop/tabels/tabulation/tabel/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('tabel_admin',tabel_admin,name='tabel_admin'),
-]
--- Desktop/tabels/tabulation/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('login/',login_user,name='login'),
-    path('logout/',logout_user,name='logout'),
-    
-]
