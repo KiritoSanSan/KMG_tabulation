0c029b995e053fe7493b845c7170fcc29d747384
KiritoSanSan <132430940+KiritoSanSan@users.noreply.github.com>
2024-05-10 17:16:53 +0600

    'spinner'

---
+++ tabulation/graph/static/graph/js/graph.js
@@ -0,0 +1,7 @@
+console.log('hello world')
+
+const spinnerBox = document.getElementById('spinner-box')
+const dataBox = document.getElementById('data-box')
+
+console.log(spinnerBox)
+console.log(dataBox)
